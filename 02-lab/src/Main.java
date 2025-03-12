5283ea2719aff2164ba0f13e678721aa6ea6a96b
zhemepatis <rink.gab@gmail.com>
2025-03-12 16:20:54 +0200

    made server to wait for less players

--- 02-lab/src/Main.java
+++ 02-lab/src/Main.java
@@ -2,7 +2,7 @@ import models.GameServer;
 import utils.PlayerTask;
 
 public class Main {
-    public static final int REQUIRED_PLAYERS = 50;
+    public static final int REQUIRED_PLAYERS = 25;
 
     public static void main(String[] args) throws Exception {
         GameServer server = new GameServer(REQUIRED_PLAYERS);
