b30e3e667fb6e1b927b747351a2f3b9e65a06728
zhemepatis <rink.gab@gmail.com>
2025-05-21 18:49:46 +0300

    updated script

--- 04-lab/scripts/gol_4.sh
+++ 04-lab/scripts/gol_4.sh
@@ -5,8 +5,13 @@
 WIDTH=32
 THREAD_NUM=5
 
+module load openmpi
 mpirun -np $THREAD_NUM ./main $WIDTH 1000 0
 echo "---"
+
+module load openmpi
 mpirun -np $THREAD_NUM ./main $WIDTH 10000 0
 echo "---"
+
+module load openmpi
 mpirun -np $THREAD_NUM ./main $WIDTH 100000 0
